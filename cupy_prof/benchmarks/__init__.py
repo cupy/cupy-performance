f7d84ea2e72fd66ae40b6fea0311b6f97e0ea2af
Emilio Castillo <ecastill@preferred.jp>
2019-12-24 04:09:38 +0000

    Added noncontiguous benchmark example

--- cupy_prof/benchmarks/__init__.py
+++ cupy_prof/benchmarks/__init__.py
@@ -1,3 +1,4 @@
 from cupy_prof.benchmarks import benchmark  # NOQA
 from cupy_prof.benchmarks import core  # NOQA
 from cupy_prof.benchmarks import ufunc  # NOQA
+from cupy_prof.benchmarks import noncontiguous  # NOQA
